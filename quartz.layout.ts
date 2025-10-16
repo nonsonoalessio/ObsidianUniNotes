69069d896baf3602170b812fec4859afbc1aece3
Alessio Donato Giura <35069388+nonsonoalessio@users.noreply.github.com>
2025-10-16 15:06:19 +0200

    Minor changes

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,6 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
@@ -43,7 +42,6 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
   ],
 }
 
